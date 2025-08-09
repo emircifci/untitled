3399b9e7aa23ca281bb4fd296edb9ab58c853f7e
Emirhan Çifçi <emirhancifci@192.168.1.121>
2025-08-09 10:16:16 +0300

    Rebuild web app with clean data for deployment

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "38e4156fa6bc26ef56dece9992ebc92d",
+const RESOURCES = {"flutter_bootstrap.js": "395deb733e086ba645b08e1feb640e0b",
 "version.json": "a3322b2f035e5345663b567001345585",
-"index.html": "39d186601ed1cea7fdfcb12a81a04650",
-"/": "39d186601ed1cea7fdfcb12a81a04650",
-"main.dart.js": "1aabccc7b6795cd0c1488b250f835b06",
+"index.html": "7e599f2f4cca6b14fcbf52b5f46ab854",
+"/": "7e599f2f4cca6b14fcbf52b5f46ab854",
+"main.dart.js": "dca719e07560b1e49730ffd102d96121",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
